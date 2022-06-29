bb2c42a9a2fb0c94bedd155a0d64c01bd21fdb03
Jaaziel-M <jaazi_nes@hotmail.com>
2022-06-28 21:28:16 -0300

    entrega desafio opcional Arrays

--- Entregable2.js
+++ Entregable2.js
@@ -6,138 +6,107 @@
 //para cada mesa se crea un registro de gastos 
 //y se cancelan los precios 
 
+//Constructor de cada mesa para el array
 
-function Pedido(puesto){
-    monto = parseInt(prompt("por favor ingrese el monto del pedido para agregarlo a la mesa: "));
-    puesto = parseFloat(puesto) + parseFloat(monto);
-    alert("El importe fue agregado a la mesa seleccionada, gracias!");
-    return puesto;
+
+
+function Puesto(CodMesa, Saldo){
+    this.CodMesa = CodMesa;
+    this.Saldo = Saldo;
 }
 
-function Disponibilidad(puesto){
-    if(puesto!=0){
-        alert("La mesa se encuentra ocupada en estos momentos, por favor asignar otra!")
-    }
-    else{
-        let opc = parseInt(prompt("la mesa se encuentra disponible! \n¿Desea realizar un pedido?\n1) Si\n2) No"))
-        if (opc == 1){
-            Pedido(puesto);
-        }
-        else{
-            return puesto;
-        }
-    }
-}
-
-const PagarMesa = (puesto) => {return parseInt(puesto) - parseInt(puesto)};
-
-function Menu(){
-    let opcion = prompt("Que acción desea realizar? \nRealizar un pedido (1)\nConsultar la disponibilidad de una mesa (2)\nCancelar el saldo pendiente de una mesa (3)\nSalir del programa (4)")
-    return opcion;
-}
-
-
-
-let Mesa1 =0;
-//let Mesa2 =0;
-//let Mesa3 =0;
-//let Mesa4 =0;
-//let Mesa5 =0;
-//let Mesa6 =0;
-//let Mesa7 =0;
-//let Mesa8 =0;
-//let Mesa9 =0;
-//let Mesa10 =0;
-//let Mesa11 =0;
-//let Puesto1 =0;
-//let Puesto2 =0;
-//let Puesto3 =0;
-//let Puesto4 =0;
-//let Puesto5 =0;
-//let Puesto6 =0;
+let Puestos = [
+    Puesto("mesa1", 0),
+    Puesto("mesa2", 0),
+    Puesto("mesa3", 0),
+    Puesto("mesa4", 0),
+    Puesto("mesa5", 0),
+    Puesto("mesa6", 0),
+    Puesto("mesa7", 0),
+    Puesto("mesa8", 0),
+    Puesto("mesa9", 0),
+    Puesto("mesa10", 0),
+    Puesto("mesa11", 0),   
+    Puesto("puesto12", 0),
+    Puesto("puesto13", 0),
+    Puesto("puesto14", 0),
+    Puesto("puesto15", 0),
+    Puesto("puesto16", 0),
+    Puesto("puesto17", 0),
+];
+let RecaudacionTotal = 0;
 var opcion =0;
 
 
-while ((opcion<1)||(opcion>17)){
-    opcion = parseInt(prompt(
-        `
-        Seleccione la posición:
-
-        Mesa 1 (1)  Mesa 6 (6)      Puesto 1 (12)
-        Mesa 2 (2)  Mesa 7 (7)      Puesto 2 (13)
-        Mesa 3 (3)  Mesa 8 (8)      Puesto 3 (14)
-        Mesa 4 (4)  Mesa 9 (9)      Puesto 4 (15)
-        Mesa 5 (5)  Mesa 10 (10)    Puesto 5 (16) 
-                    Mesa 11 (11)    Puesto 6 (17)
-        `
-    ));
+function Pedido(numero){
+    
+    monto = parseInt(prompt("por favor ingrese el monto del pedido para agregarlo a la mesa: "));
+    Puestos[parseInt(numero)-1].Saldo =  parseInt(Puestos[parseInt(numero)-1].Saldo) + monto
+    alert("La mesa: "+Puestos[parseInt(numero)-1].CodMesa+"posee un saldo de: "+Puestos[parseInt(numero)-1].Saldo);
 }
 
-//Lo hago solo con la mesa 1 para no hacer el codigo demasiado extenso
+function Disponibilidad(busqueda){
+    const puesto = Puestos.find( (puesto) => puesto.CodMesa == busqueda)
+    if(puesto){
+        alert("La mesa se encuentra en uso")
+    }
+    else{
+        alert("La mesa puede ser utilizada")
+    }
+}
+
+
+
+
+const PagarMesa = (puesto) => {parseInt(Puestos[puesto].Saldo) - parseInt(Puestos[puesto].Saldo)};
+
+
+opcion = prompt(parseInt("Por favor ingrese que consulta desea realizar:\n\nVer o pagar el saldo  de una mesa (1)\nAgregar pedido a una mesa (2)\nConsultar mesas disponibles (3)\n\nAnalytics:\n\nConsultar el total de todas las mesas en este momento (4)\nConsultar la recaudación total del día (5)"))
+
+while (opcion<1 || opcion>3){
+    opcion = prompt(parseInt("Por favor ingrese que consulta desea realizar:\n\nVer o pagar el saldo  de una mesa (1)\nAgregar pedido a una mesa (2)\nConsultar mesas disponibles (3)\n\nAnalytics:\n\nConsultar el total de todas las mesas en este momento (4)\nConsultar la recaudación total del día (5)\nSalir (6)"))
+}
 
 switch(opcion){
-
     case 1:
-        Mesa1
-        //opcion = parseInt(Menu());
-        //alert("valor de opcion"+opcion)
-        let salir = 1;
-        do{
-            opcion = parseInt(Menu());
-            alert("valor de opcion"+opcion)
-            
-            switch(opcion){
-                case 1: 
-                    alert("entro en el segundo case 1")
-                    Mesa1 = parseInt(Pedido(Mesa1))
-                    alert("EL Saldo de la mesa 1 ahora es de: $"+Mesa1)
-                    salir = parseInt(prompt("Desea salir del programa?\nSi(1)\nNo(2)"))
-                    break;
-                case 2:
-                    Mesa1 = Disponibilidad(Mesa1);
-                    salir = parseInt(prompt("Desea salir del programa?\nSi(1)\nNo(2)"))
-                    break
-                case 3:
-                    Mesa1 = PagarMesa(Mesa1);
-                    salir = parseInt(prompt("Desea salir del programa?\nSi(1)\nNo(2)"))
-                    break
-                case 4:
-                    break;
-            }
-        }while(salir!=1)
+        let lugar = prompt(parseInt("por favor ingrese un numero de mesa:\n\nMesas (Del 1 al 11)\nPuestos (Del 12 al 17)"))
+        while (lugar<1 || lugar>17){
+            lugar = prompt(parseInt("por favor ingrese un numero de mesa:\n\nMesas (Del 1 al 11)\nPuestos (Del 12 al 17)"))
+        }
+        opcion = prompt(parseInt("Desea pagar o consultar el saldo del puesto seleccionado?\nPagar saldo (1)\nConsultar saldo (2)\nSalir (3)"))
+        while(opcion < 1 || opcion >3){
+            opcion = prompt(parseInt("Desea pagar o consultar el saldo del puesto seleccionado?\nPagar saldo (1)\nConsultar saldo (2)\nSalir (3)"))
+        }
+        switch(opcion){
+            case 1:
+                parseInt(Puestos[lugar-1].Saldo)
+                RecaudacionTotal = PagarMesa(lugar-1)
+                break;
+            case 2:
+                alert("El saldo de la mesa seleccionada es: "+Puestos[lugar-1].Saldo)
+                break;
+            case 3:
+                opcion = 0;
+                break;
+        }
 
         break;
-//    case 2:
-//        let Mesa2
-//    case 3:
-//        let Mesa3
-//    case 4:
-//        let Mesa4
-//    case 5:
-//        let Mesa5
-//    case 6:
-//        let Mesa6
-//    case 7:
-//        let Mesa7
-//    case 8:   
-//        let Mesa8
-//    case 9:
-//        let Mesa9  
-//    case 10:
-//        let Mesa10 
-//    case 11:
-//        let Mesa11 
-//    case 12:
-//        let Puesto1
-//    case 13:
-//        let Puesto2
-//    case 14:
-//        let Puesto3
-//    case 15:
-//        let Puesto4
-//    case 16:
-//        let Puesto5
-//    case 17:
-//        let Puesto6
-}
+    case 2:
+        lugar = prompt(parseInt("por favor ingrese un numero de mesa:\n\nMesas (Del 1 al 11)\nPuestos (Del 12 al 17)"));
+        Pedido(lugar)
+        break;
+    case 3:
+        
+       // const Mesaslibres[] = Puestos.map(Puestos, (nombre)=>{
+       //     if(Puestos.Saldo==0){
+       //         Mesaslibres.push(nombre)
+       //     }
+       // })
+       // break;
+    case 4:
+        
+    break;
+    case 5:
 
+        break;
+}
